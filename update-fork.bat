051cb0e07ae8133da5045889d4f14c654a9ed666
Julrich Kieffer <julrich@mrkieffer.london>
2024-10-28 11:56:55 +0000

    Update upstream merging script

--- update-fork.bat
+++ update-fork.bat
@@ -1,2 +1,7 @@
+REM https://graphite.dev/guides/upstream-remote
+
 git checkout master
-git pull --rebase upstream master
+git fetch upstream
+REM git pull --rebase upstream master
+git merge upstream/master
+REM git push origin master
